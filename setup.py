9225fc7caaf253cb1d4560ca4b7c55d85963257a
Rory Maizels <rory.maizels@crick.ac.uk>
2023-05-23 14:55:35 +0100

    skeleton set up

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+version = "0.1dev"
+
+with open("README.md", encoding="UTF-8") as f:
+    readme = f.read()
+
+with open("requirements.txt", encoding="UTF-8") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="velvet",
+    version=version,
+    description="",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    author="",
+    author_email="",
+    url="",
+    license="",
+    entry_points={
+        "console_scripts": ["velvet=velvet.__main__:run_velvet"]
+    },
+    python_requires=">=3.9",
+    install_requires=required,
+    packages=find_packages(exclude="docs"),
+    include_package_data=True,
+    zip_safe=False,
+)
